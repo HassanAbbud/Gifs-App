4b0b2c68b0694b9acfae854533af8a96291922d5
Hassan Sharif Abbud González <hassanabbud@hotmail.com>
2024-07-05 11:32:54 -0700

    gifs list & query

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -14,13 +15,17 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   `
 })
 export class SearchBoxComponent {
+  constructor(private gifsService: GifsService){}
 
   @ViewChild("txtTagInput")
+
   public tagInput!: ElementRef<HTMLInputElement>;
 
   public searchTag(){
     const newTag = this.tagInput.nativeElement.value
 
-    console.log({newTag})
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = "";
   }
 }
